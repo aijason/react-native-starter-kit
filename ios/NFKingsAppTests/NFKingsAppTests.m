b434592b71a54e8ebfac697db9f2974b35742092
aijason <977854695@qq.com>
2021-06-15 19:42:53 +0800

    rename

--- ios/NFKingsAppTests/NFKingsAppTests.m
+++ ios/NFKingsAppTests/NFKingsAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeStarterKitTests : XCTestCase
+@interface NFKingsAppTests : XCTestCase
 
 @end
 
-@implementation ReactNativeStarterKitTests
+@implementation NFKingsAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
