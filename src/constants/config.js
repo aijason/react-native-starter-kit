7ca145fcf2e4c646b8a18f8333ba9111365d920c
aijason <977854695@qq.com>
2021-06-24 12:32:20 +0800

    rename wulian

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,7 +2,7 @@ const isDevEnv = process.env.NODE_ENV === 'development';
 
 export default {
   // App Details
-  appName: 'NFKings',
+  appName: 'Wulian',
 
   // Build Configuration - eg. Debug or Release?
   isDevEnv,
