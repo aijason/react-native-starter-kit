5c1ad94d4ce989d69e5cf7dddeceeb226edc895b
aijason <977854695@qq.com>
2021-06-24 12:17:19 +0800

    rename wulian

--- android/app/src/main/java/com/tiancai/wulian/MainActivity.java
+++ android/app/src/main/java/com/tiancai/wulian/MainActivity.java
@@ -1,4 +1,4 @@
-package com.tiancai.nfkings;
+package com.tiancai.wulian;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
@@ -18,6 +18,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "NFKings";
+    return "WulianApp";
   }
 }
